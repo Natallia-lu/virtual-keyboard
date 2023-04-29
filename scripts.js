2ee443df3641ec89925b5e86b81cc93f0a6154e4
Natallia-Lu <kali.laska@mail.ru>
2023-04-29 19:07:46 +0200

    feat: add interaction virtual keyboard with mouse

--- scripts.js
+++ scripts.js
@@ -14,6 +14,7 @@ textPlace.className = 'textPlace';
 textPlace.classList.add('text-place');
 keyboard.append(textPlace)
 
+
 set.forEach(elem => {
    let row = document.createElement('div')
    row.classList.add('row')
@@ -24,24 +25,98 @@ set.forEach(elem => {
     button.innerHTML = el;
     row.append(button);
 
-    if (button.innerHTML === 'Backspace' || button.innerHTML === 'Tab' || button.innerHTML === 'CapsLk' || button.innerHTML === 'Enter' || button.innerHTML === 'Shift') {
-        button.classList.add('special')
-    } else if (button.innerHTML === 'Space') {
-        button.classList.add('space')        
-    } else {
-        button.classList.add('letter')
-    }
-})
 
+    if (button.innerHTML === 'Backspace' || button.innerHTML === 'Tab' || button.innerHTML === 'CapsLk' || button.innerHTML === 'Enter' || button.innerHTML === 'Shift') {
+        button.classList.add('special');
+     } else if (button.innerHTML === 'Space') {
+        button.classList.add('space');  
+    } else {
+        button.classList.add('letter');
+    }
+
+    
+        //MouseDown 
+            button.addEventListener('mousedown', function(){
+                button.classList.toggle('active');
+
+                if (button.innerHTML === 'Backspace') {
+                    textPlace.innerHTML = textPlace.innerHTML.slice(0, -1)
+                } else if (button.innerHTML === 'Tab') {
+                    textPlace.innerHTML += '    '
+                } else if (button.innerHTML === 'Space') {
+                    textPlace.innerHTML += ' '
+                } else if (button.innerHTML === 'Enter') {
+                    textPlace.innerHTML += '\n'
+                } else if (button.innerHTML === 'Shift' || button.innerHTML === 'Ctrl' || button.innerHTML === 'Fn' || button.innerHTML === 'Win' || button.innerHTML === 'Alt' || button.innerHTML === '←' || button.innerHTML === '↑' || button.innerHTML === '→' || button.innerHTML === '↓' || button.innerHTML === 'CapsLk') {
+                    textPlace.innerHTML
+                } else {
+                textPlace.innerHTML += button.innerHTML.toLowerCase()
+                }
+            })
+
+            button.addEventListener('mouseup', function(){
+                button.classList.remove('active');
+
+            })
+
+            
+            document.body.addEventListener('keydown', function(index){
+               //UpperCase() method returns the value of the string converted to uppercase
+                    if(button.innerHTML===index.key.toUpperCase()){
+                        button.classList.toggle('active')
+                        textPlace.innerHTML += index.key.toLowerCase()
+                } else if (index.key === 'Backspace' && button.innerHTML===index.key){
+                    button.classList.toggle('active')
+                    textPlace.innerHTML = textPlace.innerHTML.slice(0, -1)
+                } else if (index.key === 'Tab' && button.innerHTML===index.key) {
+                    button.classList.toggle('active')    
+                    textPlace.innerHTML += '    '
+                } else if (index.key === ' ' && button.innerHTML === 'Space') {
+                    button.classList.toggle('active')
+                    textPlace.innerHTML += ' '
+                } else if (index.key === 'Enter' && button.innerHTML===index.key) {
+                    button.classList.toggle('active')
+                    textPlace.innerHTML += '\n'
+                }
+            })
+            
+            document.body.addEventListener('keyup', function(){
+                button.classList.remove('active');
+            })
+
+})
 });
 
 
 
 
 
+//keyboard
+/*
+document.body.addEventListener('keydown' , function(index){
+    for (let i=0 ; i <row.length ; i++) {
+   //UpperCase() method returns the value of the string converted to uppercase
+        if(row[i].innerHTML==index.key.toUpperCase()){
+            row[i].classList.toggle('active')
+        };
+    }
+//innerHTML property is part of the Document Object Model
+    p.innerHTML+=index.key
+    if(index.key=='Backspace'){
+        p.innerHTML=p.innerHTML.slice(0 , -10)
+    }
+})
+document.body.addEventListener('keyup' , function(index){
+    for(let j=0 ; j<button.length ; j++){
+        if(button[j].innerHTML == index.key.toUpperCase()){
+            button[j].classList.remove('active')
+        }
+    }
+})
+*/
 
 
 let comment = document.createElement('h2');
 comment.classList.add('h2');
-comment.innerHTML = 'Клавиатура создана в операционной системе Windows <br> Для переключения языка комбинация: левыe ctrl + alt';
-document.body.append(comment)
+comment.innerHTML = 'Клавиатура создана в операционной системе Linux <br> Для переключения языка комбинация: Win + Space';
+document.body.append(comment);
